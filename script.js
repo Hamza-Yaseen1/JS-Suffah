9518b2f648993200a4cadaa0781cf1eff185ef36
jawad <usmankhan26112007@gmail.com>
2024-02-22 21:58:21 +0500

    nested if else

---
+++ script.js
@@ -0,0 +1,33 @@
+user = prompt("Select Time 1)Morning 2)Afternoon");
+if (user==1){
+  alert("Good Morning")
+  const menu = prompt("1)Tea 2)Juice");
+  if(menu == 1){
+    alert( "Your drink is Tea. It's hot and it tastes good.");
+  }
+  else if(menu == 2){
+    alert("Your drink is Juice. It's cold and refreshing.");
+  }
+  else{
+    alert("Invalid Input");
+}
+
+  }
+else if(user==2){
+    alert("Good ")
+    const menu = prompt("1)Biryani 2)Juice");
+    if(menu == 1){
+      alert("It's hot and it tastes good.");
+    }
+    else if(menu == 2){
+      alert("Your drink is Juice. It's cold and refreshing.");
+    }
+    else{
+      alert("Invalid Input");
+  }
+  
+    }
+    else {
+        alert("Invalid Input");
+    }
+
